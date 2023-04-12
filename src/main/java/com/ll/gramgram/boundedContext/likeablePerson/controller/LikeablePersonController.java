6d7339082c26ee4ea2440302b3cc276e6e0ed07b
PARK-MI-JI <p36912@naver.com>
2023-04-12 14:29:56 +0900

    호감취소 요청을 GETã방식에서 POST방식으로 변경

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -67,7 +67,7 @@ public class LikeablePersonController {
     }
 
     @PreAuthorize("isAuthenticated()")
-    @GetMapping("/delete/{id}")
+    @PostMapping ("/delete/{id}")
     public String delete(@PathVariable Long id) {
         LikeablePerson likeablePerson = likeablePersonService.findById(id).orElse(null);
 
